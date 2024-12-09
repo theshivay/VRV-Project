0d102a546c32dff390a4ad43cfe781b17bb9ad04
Shivam Mishra <shivaymishraeee@gmail.com>
2024-12-09 12:31:12 +0530

    Some changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,6 +8,17 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/admin', authenticate, authorize(['admin']), (req, res) => {
   res.json({ message: 'Welcome, Admin!' });
+  res.send('Admin page');
+});
+
+router.get('/moderator',authenticate,authorize("admin","moderator"), (req, res) => {
+  res.json({ message: 'Welcome, Moderator!' });
+  res.send('moderator page');
+});
+
+router.get('/user',authenticate,authorize("admin","moderator","user"), (req, res) => {
+  res.json({ message: 'Welcome, User!' });
+  res.send('User page');
 });
 
 module.exports = router;
